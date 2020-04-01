0e18aea89851defef9c314c96dfd192171810c84
Fantin <fantin.brisard@isen-ouest.yncrea.fr>
2020-04-01 17:00:37 +0200

    parti ex6

--- ex6_1.py
+++ ex6_1.py
@@ -3,11 +3,15 @@ import turtle
 def drawCurve(turtle, l):
     turtle.setup(800, 400)
 
-    if l == 0:
-        turtle.forward(l/3)
-        turtle.left(45)
-    turtle.right(60)
-    drawCurve(turtle,)
+    turtle.forward(l/3)
+    turtle.left(60)
+    turtle.forward(l/3)
+    turtle.right(120)
+    turtle.forward(l/3)
+    turtle.left(60)
+    turtle.forward(l/3)
 
 
     turtle.exitonclick()
+
+drawCurve(turtle, 300)
